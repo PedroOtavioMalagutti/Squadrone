9321edb8439a6cb3745530f04721fbea630f7a85
calvinsuzuki <calvinssdcamargo@gmail.com>
2022-12-15 22:01:41 -0300

    read duty_cycle from file

--- test.py
+++ test.py
@@ -1,20 +1,24 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
+def readSpeed() :
+    # dir = "/sys/class/pwm/pwmchip8/pwm0/duty_cycle"
+    dir = './duty_cycle'
+    file = open(dir, "r")
+    duty_cycle = file.readlines()[0]
+    file.close()
+    return str(duty_cycle[0])
 
 class Serv(BaseHTTPRequestHandler):
 
     def do_GET(self):
-        if self.path == '/':
-            self.path = '/index.html'
-        try:
-            file_to_open = open(self.path[1:]).read()
-            self.send_response(200)
-        except:
-            file_to_open = "File not found"
-            self.send_response(404)
+        self.send_response(200)
+        self.send_header('content-type', 'text/html')
         self.end_headers()
-        self.wfile.write(bytes(file_to_open, 'utf-8'))
+        self.wfile.write(readSpeed().encode())
 
+def main() :
+    httpd = HTTPServer(('localhost', 8080), Serv)
+    httpd.serve_forever()
 
-httpd = HTTPServer(('192.168.0.196', 8080), Serv)
-httpd.serve_forever()
+if __name__ == '__main__' :
+    main()
