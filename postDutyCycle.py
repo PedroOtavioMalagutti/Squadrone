caa15f3befd1047632376b5f61d55ca4575b84a8
calvinsuzuki <calvinssdcamargo@gmail.com>
2022-12-16 02:09:42 -0300

    working input/read

--- postDutyCycle.py
+++ postDutyCycle.py
@@ -5,7 +5,16 @@ def readForms(body, word) :
     bd = body.decode('utf-8')
     index = bd.find(word) + len(word) + 1
     speedStr = bd[index:len(bd)]
-    return int(speedStr)
+    return speedStr
+
+def writeDutyCycle(dutyCycle) :
+    # TODO: FORMAT INPUT
+    # dir = "/sys/class/pwm/pwmchip8/pwm0/duty_cycle"
+    dir = './duty_cycle'
+    file = open(dir, 'w')
+    duty_cycle = file.writelines([dutyCycle])
+    file.close()
+    return
 
 class Serv(BaseHTTPRequestHandler):
 
@@ -22,19 +31,20 @@ class Serv(BaseHTTPRequestHandler):
         self.wfile.write(bytes(file_to_open, 'utf-8'))
 
     def do_POST(self):
-        content_length = int(self.headers['Content-Length'])
-        body = self.rfile.read(content_length)
-        self.send_response(200)
+        if self.path == '/':
+            self.path = '/index.html'
+        try:
+            content_length = int(self.headers['Content-Length'])
+            body = self.rfile.read(content_length)
+            file_to_open = open(self.path[1:]).read()
+            self.send_response(200)
+        except:
+            file_to_open = "File not found"
+            self.send_response(404)
         self.end_headers()
         
-        response = BytesIO()
-        response.write(b'This is POST request. ')
-        response.write(b'Received: ')
-        response.write(body)   
-
-        print(readForms(body, 'speed'))
-
-        self.wfile.write(response.getvalue())
+        writeDutyCycle(readForms(body, 'speed'))
+        self.wfile.write(bytes(file_to_open, 'utf-8'))
 
 def main() :
     httpd = HTTPServer(('localhost', 8080), Serv)
