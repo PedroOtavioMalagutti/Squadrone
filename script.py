e1efa477fffc2ed900feb76f87689f0c59ee95a9
calvinsuzuki <calvinssdcamargo@gmail.com>
2022-12-18 21:00:29 -0300

    css cleaning

--- script.py
+++ script.py
@@ -5,15 +5,22 @@ def read_forms(body, word) :
     """Reads a attribute from a forms structured data"""
     bd = body.decode('utf-8')
     index = bd.find(word) + len(word) + 1
-    speedStr = bd[index:]
-    print('(' + speedStr + ')')
+    if bd.find(word) == -1 :
+        return str('not found')
+    ending = bd.find('&', index)
+    if ending == -1 : 
+        data = bd[index:]
+    else :
+        data = bd[index:ending]
+    print(body)
+    print('(' + data + ')')
 
-    if not speedStr.isnumeric() :
+    if not data.isnumeric() :
         raise Exception("ERROR: BAD FORMAT")
     else :
-        speedStr = int(speedStr)
+        data = int(data)
 
-    return str(speedStr)
+    return str(data)
 
 def write_duty_cycle(dutyCycle, DBG) :    
     """Writes a value to Duty Cycle PWM control file"""
@@ -36,6 +43,8 @@ def update_index(file, target, info) :
 def post_response(data, index, DGB = 0) :
     """Main function called method of do_POST server class method"""
     speed_input = read_forms(data, 'speed') 
+    kv_input = read_forms(data, 'kv')
+    tensao_input = read_forms(data, 'Tensao')
     write_duty_cycle(speed_input, DGB)
 
     # Updates the index base file         
