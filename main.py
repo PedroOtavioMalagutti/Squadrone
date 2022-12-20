a9d1e71fb278226fe2860042ffbd9ace8c605281
Guilherme Soares <gsoares.silvestre@gmail.com>
2022-12-20 03:31:21 -0300

    f string only for py 3.6

--- main.py
+++ main.py
@@ -3,39 +3,58 @@ import traceback
 from script import *
 import sys
 import signal
+import socket
 
 LAST_SPD = '0'
 LAST_KV = '0'
 LAST_VOLTAGE = '0'
 DBG = True
 
-def post_response(data, index) :
+# function to get wifi ip address from google connection
+def get_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.settimeout(0)
+    try:
+        # doesn't even have to be reachable
+        s.connect(('10.254.254.254', 1))
+        IP = s.getsockname()[0]
+    except Exception:
+        IP = '127.0.0.1'
+    finally:
+        s.close()
+    return IP
+
+def post_response(data, index):
     """Main function called method of do_POST server class method"""
     global DBG, LAST_SPD, LAST_KV, LAST_VOLTAGE
-    speed_input = read_forms(data, 'speed') 
+    speed_input = read_forms(data, 'speed')
     kv_input = read_forms(data, 'kv')
-    voltage_input = read_forms(data, 'voltage')   
+    voltage_input = read_forms(data, 'voltage')
     # Check all variables
-    if speed_input == '' or speed_input == 'not found' :
+    if speed_input == '' or speed_input == 'not found':
         speed_input = LAST_SPD
-    else : LAST_SPD = speed_input
-    if kv_input == '' or kv_input == 'not found' :
+    else:
+        LAST_SPD = speed_input
+    if kv_input == '' or kv_input == 'not found':
         kv_input = LAST_KV
-    else : LAST_KV = kv_input
-    if voltage_input == '' or voltage_input == 'not found' :
+    else:
+        LAST_KV = kv_input
+    if voltage_input == '' or voltage_input == 'not found':
         voltage_input = LAST_VOLTAGE
-    else : LAST_VOLTAGE = voltage_input
-    
+    else:
+        LAST_VOLTAGE = voltage_input
+
     # Controls motor speed
     duty_cycle = int(speed_input)/100 * 1e6 + 1e6
     write_duty_cycle(str(int(duty_cycle)), DBG)
 
-    ## HTML UPDATE SECTION
-    # SPEED RPM 
-    index = update_index(index, 'Speed: ', int(speed_input)/100*float(kv_input)*float(voltage_input))
+    # HTML UPDATE SECTION
+    # SPEED RPM
+    index = update_index(index, 'Speed: ', int(
+        speed_input)/100*float(kv_input)*float(voltage_input))
     # RANGE SLIDER % LABEL
     index = update_index(index, '<div class="value">', speed_input)
-    # RANGE SLIDER 
+    # RANGE SLIDER
     index = update_index(index, 'value=\"', speed_input)
     # KV PLACEHOLDER
     index = update_index(index, 'name="kv" placeholder=\"', kv_input)
@@ -43,18 +62,20 @@ def post_response(data, index) :
     index = update_index(index, 'name="voltage" placeholder=\"', voltage_input)
     # VELOCIMETER LEVEL
     index = update_index(index, '000', int(speed_input))
-        
+
     # Then returns the modified webpage
     return index
 
-def signal_handler(signum, frame):  
+
+def signal_handler(signum, frame):
     """Handles SIGNAL CTRL+C to stop PWM service correctly"""
     print('Closing server...')
-    if not DBG :
-        enable = "0"    
+    if not DBG:
+        enable = "0"
         with open("/sys/class/pwm/pwmchip8/pwm0/enable", "w") as file4:
             file4.writelines([enable])
     exit(0)
+
 
 class Server(BaseHTTPRequestHandler):
     """Extends BaseHTTPRequestHandler class to create our own web server"""
@@ -93,24 +114,25 @@ class Server(BaseHTTPRequestHandler):
             index = "File not found"
             self.send_response(404)
         self.end_headers()
-        
+
         # Executes post routine
         index = post_response(data, index)
 
         # Sends a updated index page
         self.wfile.write(bytes(index, 'utf-8'))
 
-def main() :
+
+def main():
     global DBG
     # Register `SIGINT`handler (CTRL + C)
     signal.signal(signal.SIGINT, signal_handler)
 
     # Enter in DEBUG mode from terminal command line
-    # ...\Squadrone> python main.py True 
+    # ...\Squadrone> python main.py True
 
-    if len(sys.argv) == 1 :
+    if len(sys.argv) == 1:
         DBG = False
-    else :
+    else:
         DBG = sys.argv[1]
 
     # Setup values of PWM
@@ -119,7 +141,11 @@ def main() :
     duty_cycle = "1000000"
     enable = "1"
 
-    print(f"Starting Squadrone \n export = {export}, period = {period}, duty_cycle = {duty_cycle}, enable = {enable}, debug = {DBG}")
+    # print(f"Starting Squadrone \n export = {export}, period = {period}, duty_cycle = {duty_cycle}, enable = {enable}, debug = {DBG}")
+
+    # print it without fstring
+    print("Starting Squadrone \n export = %s, period = %s, duty_cycle = %s, enable = %s, debug = %s" % (
+        export, period, duty_cycle, enable, DBG))
 
     # Starts PWM Service
     if not DBG:
@@ -133,15 +159,16 @@ def main() :
             file4.writelines([enable])
 
     # Creates server
-    if DBG :
+    if DBG:
         httpd = HTTPServer(('localhost', 8080), Server)
         print("Server started at http://localhost:8080")
-    else :
+    else:
         # TODO: EXTRACT OWN IP AUTOMATICALLY
-        httpd = HTTPServer(('192.168.0.196', 8080), Server)
-        print("Server started at http://192.168.0.196:8080")
+        httpd = HTTPServer((get_ip(), 8080), Server)
+        print("Server started at http://" + get_ip() + ":8080")
 
     httpd.serve_forever()
 
-if __name__ == '__main__' :
+
+if __name__ == '__main__':
     main()
